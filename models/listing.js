c1c0462ef99df74517eb51780373bd6fa80d44fb
Hemanta <hemantabhoi16112003@gmail.com>
2024-08-03 12:57:51 +0530

    data initialized and schema modified so that data can be inserted, it was showing error before, now solved

--- models/listing.js
+++ models/listing.js
@@ -1,31 +1,43 @@
-// listing = places (i.e. apartment, flat, house, villa, hotel)
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const listingSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-    },
-    image: {
-        type: String,
-        default: "https://unsplash.com/photos/a-swimming-pool-with-lounge-chairs-and-an-umbrella-a86IntDZ9Oc",
-        set: (v) => v === "" ? "https://unsplash.com/photos/a-swimming-pool-with-lounge-chairs-and-an-umbrella-a86IntDZ9Oc" : v,
-    },
-    price: {
-        type: Number,
-    },
-    location: {
-        type: String,
-    },
-    country: {
-        type: String,
-    },
+const imageSchema = new Schema({
+  filename: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
 });
 
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: imageSchema,
+    required: true,
+    default: {
+      filename: "defaultimage",
+      url: "https://unsplash.com/photos/a-swimming-pool-with-lounge-chairs-and-an-umbrella-a86IntDZ9Oc",
+    },
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
