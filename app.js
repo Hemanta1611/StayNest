7e427fb6252c8a50e93cf68ae05ddf000b26eff0
Hemanta <hemantabhoi16112003@gmail.com>
2024-08-05 12:40:20 +0530

    edit and update route created successfully

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Listing = require("./models/listing.js");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 async function main(params) {
@@ -37,6 +38,7 @@ app.get("/testListing", async (req, res) => {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // index route:
 app.get("/listings", async (req, res) => {
@@ -65,6 +67,21 @@ app.get("/listings/:id", async(req, res) => {
     res.render("listings/show.ejs", {listing});
 });
 
+
+// edit route:
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+// update route:
+app.put("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect("/listings");
+    // res.redirect("/listings/${id}");
+});
 
 app.listen(8080, (req, res) =>{
     console.log("server is listening to port 8080");
