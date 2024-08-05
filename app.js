819f1214391cba544da0fbe55d33e79bb92e5a03
Hemanta <hemantabhoi16112003@gmail.com>
2024-08-05 12:51:37 +0530

    delete listing route created

--- app.js
+++ app.js
@@ -83,6 +83,14 @@ app.put("/listings/:id", async (req, res) => {
     // res.redirect("/listings/${id}");
 });
 
+// delete route:
+app.delete("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
+
 app.listen(8080, (req, res) =>{
     console.log("server is listening to port 8080");
 });
